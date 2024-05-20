70beff6760bb79119746bc84080fb02c7f50eb4c
Rerurate514 <rerurateyuto@gmail.com>
2024-05-21 00:29:41 +0900

    ディレクトリの変更

--- src/ui/ReactView.tsx
+++ src/ui/ReactView.tsx
@@ -1,3 +1,37 @@
-export const ReactView = () => {
-    return <h4>Heelo!</h4>;
+import { StrictMode } from "react";
+import { ItemView, WorkspaceLeaf } from "obsidian";
+import { Root, createRoot } from "react-dom/client";
+import { ReactView } from "./components/example";
+import React from "react";
+
+
+export const VIEW_TYPE_URM_DEFAULLT = "urm-default-view";
+
+export class URMView extends ItemView {
+	root: Root | null = null;
+
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	getViewType() {
+		return VIEW_TYPE_URM_DEFAULLT;
+	}
+
+	getDisplayText() {
+		return "URM Default View";
+	}
+
+	async onOpen() {
+		this.root = createRoot(this.containerEl.children[1]);
+		this.root.render(
+			<StrictMode>
+                <ReactView></ReactView>
+            </StrictMode>
+		);
+	}
+
+	async onClose() {
+		this.root?.unmount();
+	}
 }
