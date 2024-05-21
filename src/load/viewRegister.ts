0a15e6361ab3a9946f80f2883636a03967128115
Rerurate514 <rerurateyuto@gmail.com>
2024-05-21 22:46:59 +0900

    ViewRegisiterの追加

---
+++ src/load/viewRegister.ts
@@ -0,0 +1,15 @@
+import { App, ViewCreator } from "obsidian";
+import { URMView, VIEW_TYPE_URM_DEFAULLT } from "../ui/ReactView";
+import UnnotedReasonMemo from "main";
+
+export class ViewRegister{
+    constructor(private plugin: UnnotedReasonMemo) {}
+
+    async registerAllView(){
+        this.plugin.registerView(
+			VIEW_TYPE_URM_DEFAULLT,
+			(leaf) => new URMView(leaf)
+		);
+    }
+}
+
